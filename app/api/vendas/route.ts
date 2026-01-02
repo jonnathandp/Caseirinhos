14526314a97a932262b76886216cebfa9c34397a
Diniz <40609228+jonnathandp@users.noreply.github.com>
2026-01-02 12:18:08 -0300

    correção vendas

--- app/api/vendas/route.ts
+++ app/api/vendas/route.ts
@@ -26,7 +26,7 @@ export async function GET(request: NextRequest) {
       include: {
         order: {
           include: {
-            customer: true
+            cliente: true
           }
         },
         product: true
